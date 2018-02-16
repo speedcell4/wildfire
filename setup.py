f6384e2f136caf24eef75fb90649e80cd4f9433f
speedcell4 <speedcell4@gmail.com>
2018-02-17 01:14:50 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wildfire',
+    version='0.0.1',
+    packages=['wildfire'],
+    url='',
+    license='MIT',
+    author='speedcell4',
+    author_email='speedcell4@gmail.com',
+    description='training loop and utilities for agnostic framework'
+)
